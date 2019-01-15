3b6fb428deae326abe3a9ea28f4c368990614fd8
John Fulton <fulton@redhat.com>
2019-01-15 10:55:04 -0500

    default stack user password

--- box/stack.sh
+++ box/stack.sh
@@ -8,6 +8,7 @@ export SSH_OPT="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 cat <<EOF > /tmp/stack
 useradd stack
+echo "stack:redhat" | chpasswd
 echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
 mkdir /home/stack/.ssh/; chmod 700 /home/stack/.ssh/; curl $KEY_URL > /home/stack/.ssh/authorized_keys; chmod 600 /home/stack/.ssh/authorized_keys; chcon system_u:object_r:ssh_home_t:s0 /home/stack/.ssh ; chcon unconfined_u:object_r:ssh_home_t:s0 /home/stack/.ssh/authorized_keys; chown -R stack:stack /home/stack/.ssh/
