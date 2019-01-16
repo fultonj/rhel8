093caf843901dbf648e5943f3cf09c65196b9e17
John Fulton <fulton@redhat.com>
2019-01-16 17:36:18 +0000

    Switch from loopback device to disk /dev/vdb
    Due to bug 1666822 [1] we cannot use a loopback device and
    we can not use filestore because ceph-disk has been removed
    from the ceph-nautilus container.
    
    Switch to /dev/vdb though I will need to modify files in box
    directory to create it when VM is provisioned.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=1666822

--- standalone/ceph_prep.sh
+++ standalone/ceph_prep.sh
@@ -3,18 +3,12 @@
 export SSH_GIT_CHECKOUT=1
 export FETCH=/home/stack/ceph_ansible_fetch
 
-echo "creating block device and fetch directory"
-if [[ ! -e /dev/loop3 ]]; then # ensure /dev/loop3 does not exist before making it
-    command -v losetup >/dev/null 2>&1 || { sudo yum -y install util-linux; }
-    sudo dd if=/dev/zero of=/var/lib/ceph-osd.img bs=1 count=0 seek=7G
-    sudo losetup /dev/loop3 /var/lib/ceph-osd.img
-elif [[ -f /var/lib/ceph-osd.img ]]; then #loop3 and ceph-osd.img exist
-    echo "warning: looks like ceph loop device already created. Trying to continue"
-else
-    echo "error: /dev/loop3 exists but not /var/lib/ceph-osd.img. Exiting."
+echo "confirm we have block device and create fetch directory"
+if [[ ! -e /dev/vdb ]]; then
+    echo "error: /dev/vdb does not exist"
     exit 1
 fi
-sgdisk -Z /dev/loop3
+sgdisk -Z /dev/vdb
 sudo lsblk
 if [[ ! -d $FETCH ]]; then
     mkdir $FETCH
