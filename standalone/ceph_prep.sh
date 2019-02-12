4dd9edfd0bcea80249652b1b68ae0f240ac74665
John Fulton <fulton@redhat.com>
2019-02-12 21:12:14 +0000

    Use nautilus beta for ceph-ansible RPM and ceph-container
    https://buildlogs.centos.org/centos/7/storage/x86_64/ceph-nautilus/
    https://hub.docker.com/r/ceph/daemon/tags    # v4.0.0beta-nautilus
    sudo podman pull ceph/daemon:v4.0.0beta-nautilus

--- standalone/ceph_prep.sh
+++ standalone/ceph_prep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SSH_GIT_CHECKOUT=1
+export SSH_GIT_CHECKOUT=0
 export FETCH=/home/stack/ceph_ansible_fetch
 
 echo "confirm we have block device and create fetch directory"
@@ -18,17 +18,25 @@ if [[ ! -d $FETCH ]]; then
     mkdir $FETCH
 fi
 
-echo "installing latest ceph-ansible from git and symlinking it to /usr/share"
-pushd ~
-if [[ $SSH_GIT_CHECKOUT -eq 1 ]]; then
-    git clone --single-branch --branch v4.0.0beta1 git@github.com:ceph/ceph-ansible.git
-else
-    git clone --single-branch --branch v4.0.0beta1 https://github.com/ceph/ceph-ansible.git
-fi
-popd
-sudo ln -s /home/stack/ceph-ansible /usr/share/ceph-ansible
 
-echo "installing python-notario"
+echo "installing latest ceph-ansible from git and symlinking it to /usr/share"
+
+if [[ $SSH_GIT_CHECKOUT -eq 1 ]]; then
+    echo "cloning v4.0.0beta1 from github"
+    pushd ~
+    git clone --single-branch --branch v4.0.0beta1 git@github.com:ceph/ceph-ansible.git
+    popd
+    sudo ln -s /home/stack/ceph-ansible /usr/share/ceph-ansible
+else
+    echo "Add ceph-nautilus dnf repo and install ceph-ansible 4 beta"
+    sudo dnf config-manager --add-repo https://buildlogs.centos.org/centos/7/storage/x86_64/ceph-nautilus/
+    FILE=/etc/yum.repos.d/$(ls /etc/yum.repos.d/ | grep ceph)
+    sudo sed -i "/gpgcheck/d" $FILE
+    sudo sh -c "echo gpgcheck=0 >> $FILE"
+    sudo dnf install -y ceph-ansible-4.0.0-0.beta1.1.el7
+fi
+
+echo "installing python3-notario"
 # I cannot install from CBS because I need python3 notario
 # https://cbs.centos.org/koji/buildinfo?buildID=23004
 # sudo dnf install -y https://cbs.centos.org/kojifiles/packages/python-notario/0.0.14/1.el7/noarch/python2-notario-0.0.14-1.el7.noarch.rpm
--- standalone/containers.sh
+++ standalone/containers.sh
@@ -2,4 +2,4 @@
 openstack tripleo container image prepare default \
 	  --output-env-file $HOME/containers-prepare-parameters.yaml
 # use centos7 nautilus container
-sed -i 's/ceph_tag:.*/ceph_tag:\ latest-master/g' $HOME/containers-prepare-parameters.yaml
+sed -i 's/ceph_tag:.*/ceph_tag:\ v4.0.0beta-nautilus/g' $HOME/containers-prepare-parameters.yaml
