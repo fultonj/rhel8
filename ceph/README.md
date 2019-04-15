699f06667de01ff184930949e40d6f41a893b677
John Fulton <fulton@redhat.com>
2019-04-15 19:18:37 +0000

    upload example of enabling debug

--- ceph/README.md
+++ ceph/README.md
@@ -1,3 +1,20 @@
 # Ceph Debug
-
 Shell scripts and Ansible playbooks to debug a Ceph deployment which this repo deployed.
+
+## Examples
+These playbooks are not great. I just use them to easily push updates to the ceph.conf.
+
+### Enable debugging on ceph mons
+```
+bash setup_ansible.sh
+ansible-playbook-3 debug.yml --tags getconf
+ls *.conf
+sed -i '/null/d' *.conf
+ansible-playbook-3 debug.yml --tags pushconf,restart
+```
+
+- Set up ansible
+- Download the conf files
+- Remove all ines with null
+  - When /dev/null is deleted from the ceph.conf it will log to /var/log/ceph
+- Upload the conf files and restart the relevant containers
--- ceph/setup_ansible.sh
+++ ceph/setup_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EXISTING_INV=/var/lib/mistral/overcloud/tripleo-ansible-inventory.yaml
-#EXISTING_INV=/var/lib/mistral/overcloud/ceph-ansible/inventory.yml
+#EXISTING_INV=/var/lib/mistral/overcloud/tripleo-ansible-inventory.yaml
+EXISTING_INV=/var/lib/mistral/overcloud/ceph-ansible/inventory.yml
 EXISTING_KEY=/var/lib/mistral/overcloud/ssh_private_key
 NEW_INV=$(pwd)/inventory.yaml
 NEW_KEY=$(pwd)/ssh_private_key
@@ -18,7 +18,18 @@ else
     echo "Fatal: cannot find $EXISTING_KEY"
     exit 1
 fi
-echo "Running the following:"
-echo "ansible -b -i $NEW_INV --private-key $NEW_KEY all -m ping"
-ansible -b -i $NEW_INV --private-key $NEW_KEY all -m ping
+#echo "Running the following:"
+#echo "ansible -b -i $NEW_INV --private-key $NEW_KEY all -m ping"
+#ansible -b -i $NEW_INV --private-key $NEW_KEY all -m ping
 
+echo "[defaults]" > ansible.cfg
+echo "private_key_file = $NEW_KEY" >> ansible.cfg
+echo "inventory = $NEW_INV" >> ansible.cfg
+echo "retry_files_enabled = False" >> ansible.cfg
+echo "" >> ansible.cfg
+echo "[ssh_connection]" >> ansible.cfg
+echo "ssh_args = -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" >> ansible.cfg
+
+echo "Running the following:"
+echo "ansible all -m ping"
+ansible all -m ping
